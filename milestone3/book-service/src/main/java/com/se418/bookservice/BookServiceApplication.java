b603fcc91b7e3db89a84176c2826022185522310
blasi7 <34480825+blasi7@users.noreply.github.com>
2019-06-11 05:26:37 -0700

    book-service

--- milestone3/book-service/src/main/java/com/se418/bookservice/BookServiceApplication.java
+++ milestone3/book-service/src/main/java/com/se418/bookservice/BookServiceApplication.java
@@ -1,45 +1,61 @@
 package com.se418.bookservice;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.se418.bookservice.mapper.BookMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
 public class BookServiceApplication {
-    @Value("${eureka.instance.instance-id}")
-    private String instanceId;
+//    @Value("${eureka.instance.instance-id}")
+//    private String instanceId;
 
     public static void main(String[] args) {
         SpringApplication.run(BookServiceApplication.class, args);
     }
  
-    private List<Book> bookList = Arrays.asList(
-        new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
-        new Book(2L, "Baeldung goes to the park", "Slavisa")
-    );
+//    private List<Book> bookList = Arrays.asList(
+//        new Book(1L, "Baeldung goes to the market", "Tim Schimandle"),
+//        new Book(2L, "Baeldung goes to the park", "Slavisa")
+//    );
 
-    @GetMapping("/")
-    public String getinstanceID(){
-        return "Hi there! instance id: " + instanceId;
-    }
- 
+//    @GetMapping("/")
+//    public String getinstanceID(){
+//        return "Hi there! instance id: " + instanceId;
+//    }
+    @Autowired
+    BookMapper bookMapper;
     @GetMapping("/books")
     public List<Book> findAllBooks() {
-        return bookList;
+        return bookMapper.selectAll();
     }
  
     @GetMapping("/books/{bookId}")
     public Book findBook(@PathVariable Long bookId) {
-        return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
+        return bookMapper.selectById(bookId).orElse(null);
     }
+
+    @PostMapping("/remove/{bookId}")
+    public void deleteBook(@PathVariable long BookId) {
+        bookMapper.deleteById(BookId);
+    }
+    @PostMapping("/insert")
+    private void insertBook(
+            @RequestBody Book book
+    ) {
+        bookMapper.insertBook(book);
+    }
+    @PostMapping("/update")
+    private void updateBook(
+            @RequestBody Book book
+    ){
+        bookMapper.updateBook(book);
+    }
+
 }
