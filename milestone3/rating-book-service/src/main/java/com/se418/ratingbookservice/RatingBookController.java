3c866e3d4512ef95787b3e234d339caad79d7ae2
bald-win <1351533120@qq.com>
2019-06-11 20:49:39 +0800

    rating server

---
+++ milestone3/rating-book-service/src/main/java/com/se418/ratingbookservice/RatingBookController.java
@@ -0,0 +1,49 @@
+package com.se418.ratingbookservice;
+
+import com.se418.ratingbookservice.entity.Book;
+import com.se418.ratingbookservice.entity.Rate;
+import com.se418.ratingbookservice.repository.RateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.http.HttpSession;
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+@RestController
+public class RatingBookController {
+    @Autowired
+    private RateRepository rateRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping("/rate")
+    public String rate(@RequestParam long userId, @RequestParam long bookId, @RequestParam int point) {
+        //get current time
+        Calendar calendar = Calendar.getInstance();
+        Timestamp now = new Timestamp(calendar.getTimeInMillis());
+
+        //check legality
+        Book book = restTemplate.getForObject("http://book-server/books/" + bookId, Book.class);
+        if (book == null) return "rate failed";
+        HttpSession session = null;
+        String userCheck = restTemplate.postForObject("http://book-server/login/check_session", session, String.class);
+        if (userCheck == "failure") return "rate failed";
+
+        Rate rate = new Rate(userId, bookId, point, now);
+        rateRepository.save(rate);
+        return "rate succeeded";
+    }
+}
--- milestone3/rating-book-service/src/main/java/com/se418/ratingbookservice/RatingBookServiceApplication.java
+++ milestone3/rating-book-service/src/main/java/com/se418/ratingbookservice/RatingBookServiceApplication.java
@@ -8,33 +8,34 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
-@RibbonClient(name="server", configuration = RibbonConfiguration.class)
+@RibbonClient(name = "server", configuration = RibbonConfiguration.class)
 public class RatingBookServiceApplication {
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private RestTemplate restTemplate;
 
-	@LoadBalanced
-	@Bean
-	RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	@GetMapping("/")
-	public String handleRequest() {
+//    @GetMapping("/")
+//    public String handleRequest(@RequestParam long id, @RequestParam int rate) {
+//
+//		String serviceID = restTemplate.getForObject("http://book-service/", String.class);
+//
+//        return serviceID;
+//    }
 
-		String serviceID = restTemplate.getForObject("http://book-service/", String.class);
-
-		return serviceID;
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(RatingBookServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RatingBookServiceApplication.class, args);
+    }
 
 }
