8acc10b7f890b73f10a9fbad6ba1ec45fb9bba4e
blasi7 <blasi@vip.qq.com>
2019-06-13 20:14:05 +0800

    rm

--- milestone2/rating-book-service/src/main/java/com/se418/ratingbookservice/repository/RateRepository.java
+++
@@ -1,9 +0,0 @@
-package com.se418.ratingbookservice.repository;
-
-import com.se418.ratingbookservice.entity.Rate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RateRepository extends JpaRepository<Rate,Long> {
-}
--- milestone3/user-service/src/main/java/com/se418/userservice/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.se418.userservice;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-}
